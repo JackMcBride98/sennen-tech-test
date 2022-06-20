2a2e5173fbbd12f2f9b72ded2ba2e147ed13303f
Jack McBride <jack.mcbride@sihealth.co.uk>
2022-06-20 09:12:32 +0100

    API back up and working so able to test properly

--- index.js
+++ index.js
@@ -5,29 +5,34 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-var randomCoordinates = new Array(0).fill(0).map(function () { return ({
+var randomCoordinates = new Array(100).fill(0).map(function () { return ({
     lat: getRandomIntInclusive(-90, 90),
     lng: getRandomIntInclusive(-180, 180)
 }); });
 var sunriseSunsetData = [];
 var fetchSunriseSunsetData = function (item, callback) {
-    fetch("https://api.sunrise-sunset.org/json?lat=".concat(item === null || item === void 0 ? void 0 : item.lat, "&lng=").concat(item === null || item === void 0 ? void 0 : item.lng))
+    fetch("https://api.sunrise-sunset.org/json?lat=".concat(item === null || item === void 0 ? void 0 : item.lat, "&lng=").concat(item === null || item === void 0 ? void 0 : item.lng, "&formatted=0"))
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        sunriseSunsetData.push(data);
+        sunriseSunsetData.push(data.results);
+        callback();
     })["catch"](function (err) {
         console.log(err);
+        callback();
     });
-    callback();
 };
 async.eachLimit(randomCoordinates, 5, fetchSunriseSunsetData, function (err) {
     if (err) {
         console.log(err);
     }
-    var earliestSunrise = sunriseSunsetData.reduce(function (prev, curr) {
+    var filteredSunriseSunsetData = sunriseSunsetData.filter(function (item) {
+        return item.sunrise !== '1970-01-01T00:00:00+00:00' &&
+            item.sunrise !== '1970-01-01T00:00:01+00:00';
+    });
+    var earliestSunrise = filteredSunriseSunsetData.reduce(function (prev, curr) {
         return new Date(prev.sunrise).getTime() < new Date(curr.sunrise).getTime()
             ? prev
             : curr;
-    }, sunriseSunsetData[0]);
+    });
     console.log(earliestSunrise);
 });
--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ function getRandomIntInclusive(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const randomCoordinates: Coordinates[] = new Array(0).fill(0).map(() => ({
+const randomCoordinates: Coordinates[] = new Array(100).fill(0).map(() => ({
   lat: getRandomIntInclusive(-90, 90),
   lng: getRandomIntInclusive(-180, 180),
 }));
@@ -26,27 +26,34 @@ const randomCoordinates: Coordinates[] = new Array(0).fill(0).map(() => ({
 const sunriseSunsetData: SunriseSunset[] = [];
 
 const fetchSunriseSunsetData = function (item, callback) {
-  fetch(`https://api.sunrise-sunset.org/json?lat=${item?.lat}&lng=${item?.lng}`)
+  fetch(
+    `https://api.sunrise-sunset.org/json?lat=${item?.lat}&lng=${item?.lng}&formatted=0`
+  )
     .then((response) => response.json())
     .then((data: any) => {
-      sunriseSunsetData.push(data);
+      sunriseSunsetData.push(data.results);
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      callback();
     });
-  callback();
 };
 
 async.eachLimit(randomCoordinates, 5, fetchSunriseSunsetData, function (err) {
   if (err) {
     console.log(err);
   }
-  const earliestSunrise = sunriseSunsetData.reduce(
-    (prev, curr) =>
-      new Date(prev.sunrise).getTime() < new Date(curr.sunrise).getTime()
-        ? prev
-        : curr,
-    sunriseSunsetData[0]
+  const filteredSunriseSunsetData = sunriseSunsetData.filter(
+    (item) =>
+      item.sunrise !== '1970-01-01T00:00:00+00:00' &&
+      item.sunrise !== '1970-01-01T00:00:01+00:00'
+  );
+
+  const earliestSunrise = filteredSunriseSunsetData.reduce((prev, curr) =>
+    new Date(prev.sunrise).getTime() < new Date(curr.sunrise).getTime()
+      ? prev
+      : curr
   );
   console.log(earliestSunrise);
 });
